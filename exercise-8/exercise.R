06ec5fc0089fe2c583333cf00106a28de3bd213b
Sitong Wu <Sitong Wu>
2017-04-19 14:19:58 -0700

    in lab

--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -11,21 +11,21 @@ library(dplyr)
 
 # View in the data set. Start to understand what the data columns contains
 # Be sure to comment out the function so it won't view everytime you run the code.
-
+View(pulitzer)
 
 # Use 'colnames' to print out the names of the columns
-
+colnames(pulitzer)
 
 # Use 'str' to print what types of values are contained in each column
 # Did any value type surprise you? Why do you think they are that type?
-
+str(pulitzer)
 
 # Add a column that contains the diffrence in changes in Daily Circulation from 2004 to 2013
-
+pulitzer <- mutate(pulitzer, diffrence.in.2004.2013 = Daily.Circulation..2013 - Daily.Circulation..2004)
 
 # What publication gained the most pulitzer prizes from 2004-2014?
 # Be sure to use the pipe operator! 
-
+greatest <- pulitzer %>%  filter(Pulitzer.Prize.Winners.and.Finalists..2004.2014 == max(Pulitzer.Prize.Winners.and.Finalists..2004.2014)) %>% select(Newspaper)
 
 # Which publication with at least 5 Pulitzers won from 2004-2014 had the biggest decrease(negative) in Daily circulation numbers? 
 # This publication should have Pulitzer prizes won a minimum of 5 Pulitzers, as well as the biggest decrease in circulation
